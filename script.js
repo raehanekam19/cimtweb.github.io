f19466f4fa6f2880c04149a935f22d87c98b0d01
raehanekam19 <raehanekamaolana19@gmail.com>
2024-12-15 00:36:26 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,5 @@
+   btnbar = document.querySelector(".btnbar");
+   btnbar.onclick = function() {
+      navbarBar = document.querySelector(".navbar");
+      navbarBar.classList.toggle("active");
+}
